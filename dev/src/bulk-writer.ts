bd70bfb05b74f3961f230311412b2dfbc8b1bc73
Brian Chen <chenbrian@google.com>
2020-09-23 09:49:51 -0500

    fix: fix flaky BulkWriter rate limiter test

--- dev/src/bulk-writer.ts
+++ dev/src/bulk-writer.ts
@@ -727,4 +727,14 @@ export class BulkWriter {
   _setMaxBatchSize(size: number): void {
     this.maxBatchSize = size;
   }
+
+  /**
+   * Sets the rate limiter for testing.
+   *
+   * @private
+   */
+  // Visible for testing.
+  _setRateLimiter(limiter: RateLimiter): void {
+    this.rateLimiter = limiter;
+  }
 }
